eaaee33f272a7a2a2c9b676f97b162ff8df805e6
Hachemi Harizi <hachemi@protonmail.com>
2021-04-12 09:21:11 +0200

    add insertion-sort.js

---
+++ sorts/insertion-sort/insertion-sort-for.js
@@ -0,0 +1,23 @@
+const arr = [3, 45, 67, 98, 78, 25, 17];
+
+function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1;
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+}
+
+
+console.log("-----before sorting-----");
+console.log(arr);
+console.log("-----after sorting-----");
+console.log(insertionSort(arr));
